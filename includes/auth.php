35af2f0150b999d07e1a97f485938f7e771cbe2c
JcDevToledoHot <jcdevtoledo@outlook.com>
2026-07-10 09:12:59 -0300

    V5.0

---
+++ includes/auth.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * SeederLinux Lite - Auth Include
+ *
+ * This file is a stub - auth functions are in functions.php
+ */
+
+// Auth functions are defined in lib/functions.php
+// This file is kept for compatibility
